8e0de87421f9482bc8dc8d664d8a552d01de325a
Iz-Dine Amadou <2088346@talnet.nl>
2019-12-11 10:42:06 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <form >
+<p>E-mail</p>
+<p>
+<input type="text" placeholder="Wat is je E-mail" name="E-mail">
+<input type="submit" name="button" value="Verstuur">  </input>
+</form>
+<?php
+
+ ?>
+  </body>
+</html>
